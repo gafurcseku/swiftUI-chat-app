e084e5bd4da145e4c23663937a8c568588fe920c
Md Abdul Gafur <gafurcseku>
2024-03-08 02:47:09 +0600

    update code

---
+++ SwiftUI-Chat-App/Code/Features/ChatRoomsDetails/Screens/ReceiverRow.swift
@@ -0,0 +1,18 @@
+//
+//  ReceiverRow.swift
+//  SwiftUI-Chat-App
+//
+//  Created by Md Abdul Gafur on 8/3/24.
+//
+
+import SwiftUI
+
+struct ReceiverRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReceiverRow()
+}
---
+++ SwiftUI-Chat-App/Code/Features/ChatRoomsDetails/Screens/SenderRow.swift
@@ -0,0 +1,18 @@
+//
+//  SenderRow.swift
+//  SwiftUI-Chat-App
+//
+//  Created by Md Abdul Gafur on 8/3/24.
+//
+
+import SwiftUI
+
+struct SenderRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SenderRow()
+}
